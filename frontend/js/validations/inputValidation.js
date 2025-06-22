21d488cb9b1f271eb9f81ba5413f615299a0c63c
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-06-22 16:49:40 -0300

    add function nameValidate

--- frontend/js/validations/inputValidation.js
+++ frontend/js/validations/inputValidation.js
@@ -1,1 +1,21 @@
-// ...
+
+
+
+function nameValidate() {
+    let nameInput = document.getElementById("name-input").value
+    const regexName = new RegExp("^[a-zA-Z\\s]+$") 
+    
+
+    if (nameInput.length < 3) {
+        alert("Ingrese un nombre valido")
+        return
+    }
+
+
+    if (nameInput === "" || !regexName.test(nameInput)) { //.test devuelve true o false según si el texto coincide con la RegEx.
+        alert("Ingrese un nombre valido")
+        return
+    }
+
+    window.location.href = "pages/client/products.html";
+}
