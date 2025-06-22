c272ea47a2d6513ad7b1cd226dd5bb1b3ab96e71
Osvaldx <romanonahuel24@gmail.com>
2025-06-22 19:58:17 -0300

    add color to validations

--- frontend/js/validations/inputValidation.js
+++ frontend/js/validations/inputValidation.js
@@ -1,19 +1,25 @@
 
-
+const changeColorInput = (input) => {
+    input.style.color = "#cc4343";
+    input.style.borderBlockColor = "#cc4343";
+    setTimeout(() => {
+        input.style.color = "white";
+        input.style.borderBlockColor = "rgb(107, 107, 107)";
+    }, 2000);
+}
 
 function nameValidate() {
-    let nameInput = document.getElementById("name-input").value
-    const regexName = new RegExp("^[a-zA-Z\\s]+$") 
+    let input = document.getElementById("name-input");
+    let nameInput = input.value;
+    const regexName = new RegExp("^[a-zA-Z\\s]+$")
     
-
     if (nameInput.length < 3) {
-        alert("Ingrese un nombre valido")
+        changeColorInput(input);
         return
     }
-
-
-    if (nameInput === "" || !regexName.test(nameInput)) { //.test devuelve true o false según si el texto coincide con la RegEx.
-        alert("Ingrese un nombre valido")
+    
+    if (nameInput == "" || !regexName.test(nameInput)) { //.test devuelve true o false según si el texto coincide con la RegEx.
+        changeColorInput(input);
         return
     }
 
