c1f27c3fe63bd3432f959d2e83d36c2f19136d46
Osvaldx <romanonahuel24@gmail.com>
2025-06-24 20:19:11 -0300

    add get products in the api

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,30 @@
-const express = require("express");
-const mysql = require("mysql");
+import express from "express";
+import environments from "./src/api/config/environments.js";
+import connection from "./src/api/database/db.js";
+import cors from "cors";
+
+const app = express();
+const PORT = environments.port;
+
+// CORS basico para permitir solicitudes
+app.use(cors());
+
+try {
+    app.get("/products", async(req, res) => {
+        let sql = "SELECT * FROM productos"
+        const [rows] = await connection.query(sql);
+        
+        res.status(200).json({
+            payload: rows,
+            message: (rows.length === 0) ? "[-] No se encontraron productos" : "[+] Productos encontrados"
+        })
+    })
+} 
+catch(error) {
+    res.status(500).json({error: "[!] Error interno del servidor al obtener productos"})
+    console.log("Error: " + error)
+}
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}/`)
+})
