4b029e9848235af856baa29e863fa739e5edea94
Osvaldx <romanonahuel24@gmail.com>
2025-06-26 20:03:47 -0300

    fix type

--- backend/server.js
+++ backend/server.js
@@ -44,34 +44,33 @@ app.get("/products", async(req, res) => {
         res.status(500).json({error: "[!] Error interno del servidor al obtener productos"})
         console.log("Error: " + error)
     }
-})
+});
 // ------------------------------------------------------------------------------------------ //
 
 // ------------------------------------------------------------------------------------------ //
 // GET PRODUCT ID
 
-try {
-    app.get("/products/:id",validateID, async(req,res) => {
-            let sql = "SELECT * FROM productos WHERE id_product = ?" //-> se usa ? pq al poner el parametro "id" es vulnerable a sqlinjections
-            let { id } = req.params  //-> desestructuracion a un objeto
-            const [rows] = await connection.query(sql, [id]);
+app.get("/products/:id",validateID, async(req,res) => {
+    try {
+        let sql = "SELECT * FROM productos WHERE id_product = ?" //-> se usa ? pq al poner el parametro "id" es vulnerable a sqlinjections
+        let { id } = req.params  //-> desestructuracion a un objeto
+        const [rows] = await connection.query(sql, [id]);
 
-            res.status(200).json({
-                payload: rows,
-                message: (rows.length === 0) ? "[-] No se encontro producto con ese ID" : "[+] Producto encontrado"
-            })
+        res.status(200).json({
+            payload: rows,
+            message: (rows.length === 0) ? "[-] No se encontro producto con ese ID" : "[+] Producto encontrado"
         })
-
     } catch (error) {
         res.status(500).json({error: "[!] Error interno del servidor al obtener productos"})
         console.log("Error: " + error)
     }
+});
 // ------------------------------------------------------------------------------------------ //
 
 // ------------------------------------------------------------------------------------------ //
 // INSERT PRODUCT
-try {
-    app.post("/addproduct", async(req, res) => {
+app.post("/addproduct", async(req, res) => {
+    try {
         let { nombre, imagen, precio, categoria, activo } = req.body
         let sql = `INSERT INTO productos (nombre, imagen, precio, categoria, activo) VALUES (?, ?, ?, ?, ?)`
 
@@ -87,17 +86,17 @@ try {
         return res.status(200).json({
             message: (rows.length === 0) ? "[-] NO SE AGREGO EL PRODUCTO" : "[+] PRODUCTO AGREGADO"
         })
-    })
-} catch (error) {
-    res.status(500).json({error: "[!] ERROR INTERNO DEL SERVIDOR"})
-    console.log("Error: " + error)
-}
+    } catch (error) {
+        res.status(500).json({error: "[!] ERROR INTERNO DEL SERVIDOR"})
+        console.log("Error: " + error)
+    }
+});
 // ------------------------------------------------------------------------------------------ //
 
 // ------------------------------------------------------------------------------------------ //
 // DELETE PRODUCT
-try {
-    app.delete("/deleteproduct/:id",validateID, async(req, res) => {
+app.delete("/deleteproduct/:id",validateID, async(req, res) => {
+    try {
         let { id } = req.params;
         let sql = "DELETE FROM productos WHERE id_product = ?";
         let [rows] = await connection.query(sql, [id]);
@@ -111,16 +110,15 @@ try {
                 message: "[+] Producto eliminado"
             });
         }
-    })
-
-} catch (error) {
-    res.status(500).json({error: "[!] ERROR INTERNO DEL SERVIDOR"})
-    console.log("Error: " + error)
-}
+    } catch (error) {
+        res.status(500).json({error: "[!] ERROR INTERNO DEL SERVIDOR"})
+        console.log("Error: " + error)
+    }
+});
 // ------------------------------------------------------------------------------------------ //
 // UPDATE PRODUCT
-try {
-    app.post("/updateProduct", async(req, res) => {
+app.post("/updateProduct", async(req, res) => {
+    try {
         let { id_product, nombre, imagen, precio, categoria, activo } = req.body;
 
         let results = parametersValidation(nombre,imagen,precio,categoria,activo);
@@ -137,11 +135,11 @@ try {
             message: (rows.affectedRows === 0) ? "[!] No se encontro un producto con ese ID" : "[+] Producto actualizado con exito!"
         })
 
-    });
-} catch (error) {
-    res.status(500).json({error: "[!] ERROR INTERNO DEL SERVIDOR"})
-    console.log("Error: " + error)
-}
+    } catch (error) {
+        res.status(500).json({error: "[!] ERROR INTERNO DEL SERVIDOR"})
+        console.log("Error: " + error)
+    }
+});
 // ------------------------------------------------------------------------------------------ //
 
 app.listen(PORT, () => {
