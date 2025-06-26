51c3d43fe00035f35077d8529688739da0409777
Osvaldx <romanonahuel24@gmail.com>
2025-06-26 19:34:48 -0300

    add new validation: activo

--- backend/src/api/validations/serverValidations.js
+++ backend/src/api/validations/serverValidations.js
@@ -1,44 +1,45 @@
-const parametersValidation = (nombre,imagen,precio,categoria) => {
+const parametersValidation = (nombre,imagen,precio,categoria, activo) => {
     const regEx = new RegExp("^https://[a-zA-Z0-9].{3,}")
     let result = {
         allow: true,
         message: ""
     }
-    
-    if(!nombre){
+
+    if(!nombre || !imagen || !precio || !categoria){
         result.allow = false;
-        result.message = "El nombre no puede ser null o vacio"
+        result.message = "[!] Debe completarse todos los campos"
         
         return result
     }
 
-    if(!regEx.test(imagen) || !imagen) {
+    if(!regEx.test(imagen)) {
         result.allow = false;
-        result.message = "La imagen no respeta el formato o esta vacio"
+        result.message = "[!] La imagen no respeta el formato o esta vacio"
         
         return result
     }
 
     if(precio < 0) {
         result.allow = false;
-        result.message = "El precio no puede ser negativo"
+        result.message = "[!] El precio no puede ser negativo"
         
         return result
     }
 
     if(categoria != "SKATE" && categoria != "ROLLER") {
         result.allow = false;
-        result.message = "La categoria no es correcta"
+        result.message = "[!] La categoria no es correcta"
         
+        return result
+    }
+
+    if(activo != true && activo != false) {
+        result.allow = false;
+        result.message = "[!] El activo no es un booleano"
+
         return result
     }
     
     return result
 }
-
-const isParameter = (param) => {
-    const parameters = ["nombre","imagen","precio","categoria","activo"];
-    return (parameters.includes(param))
-}
-
-export default { parametersValidation,isParameter };
+export default parametersValidation;
