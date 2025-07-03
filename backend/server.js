d70fa30f121a7bf0e4ac4b86e70880ef6ac092c8
osvaldx <romanonahuel24@gmail.com>
2025-07-03 20:08:23 -0300

    add configuration and implementations views

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,35 @@ import express from "express";
 import environments from "./src/api/config/environments.js";
 import cors from "cors";
 import morgan from "morgan";
-import { productRouter } from "./src/api/routers/index.js";
+import { productRouter } from "./src/api/routes/index.js";
+import { join, __dirname } from "./src/api/utils/index.js";
 
 const app = express();
 const PORT = environments.port;
+
+// Configuramos el EJS como motor de plantillas
+app.set("view engine", "ejs");
+
+// Definimos la ruta donde esta almacenada las plantillas EJS con join
+// combinamos el directorio raiz del proyecto con src/views
+app.set("views", join(__dirname, "src/views"));
+
+// Configuramos express para servir archivos estaticos desde la carpeta public
+app.use(express.static(join(__dirname, "src/public")));
 
 //MIDDLEWARES
 app.use(cors()); // MIDDLEWARE basico para permitir solicitudes
 app.use(express.json()); // MIDDLEWARE PARA PODER RECIBIR COSAS DEL BODY EN EL POST
 app.use(morgan("dev")) // MIDDLEWARE para registrar los metodos http
+
+// VISTAS
+app.get("/dashboard", (req, res) => {
+    res.render("index");
+})
+
+app.get("/searchProduct", (req, res) => {
+    res.render("searchProduct");
+})
 
 // ------------------------------------------------------------------------------------------ //
 app.use("/api", productRouter);
