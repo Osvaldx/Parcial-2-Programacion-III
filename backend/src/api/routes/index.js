3e1b5a3f5b2e60572759929ededec7766472ffae
osvaldx <romanonahuel24@gmail.com>
2025-07-04 20:38:30 -0300

    add views.routers.js

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,6 +1,8 @@
 import productRouter from "./product.routers.js";
+import viewRouter from "./views.routers.js";
 
 // Archivo de barril con todas las rutas
 export {
-    productRouter
+    productRouter,
+    viewRouter
 }
