8634da6b876cae485e438b987925c10e4c5ee0b5
osvaldx <romanonahuel24@gmail.com>
2025-07-04 20:41:31 -0300

    add logic of the routes

---
+++ backend/src/api/controllers/viewControllers.js
@@ -0,0 +1,30 @@
+import Products from "../models/product.models.js";
+
+export const listView = async (req, res) => {
+    try {
+        const listProducts = await Products.selectAllProducts();
+
+        res.render("index", {
+            products: listProducts[0]
+        });
+    }
+    catch(error) {
+
+    }
+}
+
+export const searchProductIDView = async (req, res) => {
+    res.render("searchProduct");
+}
+
+export const addProductView = async (req, res) => {
+    res.render("addProduct");
+}
+
+export const updateProductView = async (req, res) => {
+    res.render("updateProduct");
+}
+
+export const deleteProductView = async (req, res) => {
+    res.render("deleteProduct");
+}
