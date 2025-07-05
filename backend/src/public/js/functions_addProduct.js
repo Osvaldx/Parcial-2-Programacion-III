6f64db65adddae48d82a50b55c5f2e33a39190d2
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-07-05 02:11:26 -0300

    add products function completed

--- backend/src/public/js/functions_addProduct.js
+++ backend/src/public/js/functions_addProduct.js
@@ -1,4 +1,4 @@
-import { parametersValidation } from "../../../src/public/js/parametersValidations";
+import { parametersValidation } from "/js/parametersValidations.js";
 
 // Variables
 const ElementFormSearch = document.getElementById("form-addProduct");
@@ -7,39 +7,54 @@ const listProduct = document.getElementById("list-products")
 
 // Funciones
 
-const addProduct = async(event) => {
+let addProductAPI = async(product) => {
+    let response = await fetch("http://localhost:3000/api/products/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+    })
+    
+    return response
+}
+
+
+const showResultsAddProduct = async(event) => {
     let cardProduct = ""
     event.preventDefault(); // Evita que la pagina se recargue al enviar el form
-
+    
     let formData = new FormData(event.target); // Lee los datos del form y los devuelve
     let data = Object.fromEntries(formData.entries()); // Convierte el FormData a objeto js
-
+    
     let activo = (data.activeProduct == "si") ? true : false
     let result = parametersValidation(data.nameProduct, data.imageProduct, data.priceProduct, data.categoryProduct.toUpperCase(), activo)
-
-
+    
+    
     if(result.allow){
-        cardProduct += `<li class="card-product">
-                <div class="img-box">
-                    <img src="${data.imageProduct}" alt="">
-                </div>
-                <h4>${data.nameProduct}</h4>
-                <p>$${data.priceProduct}</p>
-                <p>${data.categoryProduct}</p>
-                <p>ACTIVO: <strong>${activo}</strong></p>
-            </li>`;
+        let product = {
+                "nombre": data.nameProduct,
+                "imagen": data.imageProduct,
+                "precio": data.priceProduct,
+                "categoria": data.categoryProduct,
+                "activo": activo
+        }
+
+        let response = await addProductAPI(product)
+        if ((await response).ok) {
+            cardProduct += `<p>El producto fue agregado con exito</p>`;
+        }else{
+            cardProduct += `<p>ERROR AL AGREGAR UN PRODUCTO!</p>`;
+        }
     }else{
         cardProduct += `<p>${result.message}</p>`
     }
 
     listProduct.innerHTML = cardProduct
-    console.log(data)
-
-
 }
 
 // EVENTOS
-ElementFormSearch.addEventListener("submit", addProduct);
+ElementFormSearch.addEventListener("submit", showResultsAddProduct);
 
 // INICIALIZADOR
 const Init = () => {
