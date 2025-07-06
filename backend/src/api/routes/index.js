32e0504ad3cb64ff55920721395566d918da53bb
osvaldx <romanonahuel24@gmail.com>
2025-07-06 15:27:21 -0300

    add adminRouter and export

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,8 +1,10 @@
 import productRouter from "./product.routers.js";
 import viewRouter from "./views.routers.js";
+import adminRouter from "./adminAccount.router.js";
 
 // Archivo de barril con todas las rutas
 export {
     productRouter,
-    viewRouter
+    viewRouter,
+    adminRouter
 }
