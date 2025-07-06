94d350dccc03f0d8262352f275242c1b67379c49
osvaldx <romanonahuel24@gmail.com>
2025-07-06 18:02:57 -0300

    add /admin/getAllAdmins

--- backend/src/api/routes/adminAccount.router.js
+++ backend/src/api/routes/adminAccount.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import middlewares from "../middlewares/middlewares.js";
-import { createAdminAccount, deleteAdminAccount, checkAdminAccount } from "../controllers/adminControllers.js";
+import { createAdminAccount, deleteAdminAccount, checkAdminAccount, getAllAdminAccounts } from "../controllers/adminControllers.js";
 // El resto de las funciones que vienen de los controladores
 
 const router = Router();
+
+router.get("/admin/getAllAdmins", getAllAdminAccounts);
 
 router.post("/admin/checkAdmin", checkAdminAccount);
 
