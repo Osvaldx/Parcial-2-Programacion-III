554356bab25d9c021418c903f2fa4be88c2d5a9a
osvaldx <romanonahuel24@gmail.com>
2025-07-06 18:04:34 -0300

    add three functions new, deleteAdminAccount, checkAdminAccount, getAllAdminAccounts

--- backend/src/api/controllers/adminControllers.js
+++ backend/src/api/controllers/adminControllers.js
@@ -26,33 +26,62 @@ export const createAdminAccount = async(req, res) => {
 };
 
 export const deleteAdminAccount = async(req, res) => {
-    let { id } = req.params;
-    if(!adminValidations.ValidateID(id)) {
-        return res.status(400).json({
-            message: "[!] ID no valida"
+    try {
+        let { id } = req.params;
+        if(!adminValidations.ValidateID(id)) {
+            return res.status(400).json({
+                message: "[!] ID no valida"
+            })
+        }
+        
+        let [rows] = await Admins.deleteAdmin(id);
+        
+        res.status(200).json({
+            message: (rows.length == 0) ? `[!] NO SE PUDO ELIMINAR LA CUENTA CON ID: ${id}` : `[+] LA CUENTA ID ${id} SE ELIMINO CON EXITO!`
         })
+    } catch(error) {
+        res.status(500).json({
+            error: "[!] ERROR INTERNO DEL SERVIDOR"
+        })
+        console.log(`ERROR: ${error}`);
     }
-    
-    let [rows] = await Admins.deleteAdmin(id);
-    
-    res.status(200).json({
-        message: (rows.length == 0) ? `[!] NO SE PUDO ELIMINAR LA CUENTA CON ID: ${id}` : `[+] LA CUENTA ID ${id} SE ELIMINO CON EXITO!`
-    })
 }
 
 export const checkAdminAccount = async(req, res) => {
-    let { username, password } = req.body;
-    let result = adminValidations.parametersValidation(null, username, password);
-
-    if(!result.allow) {
-        return res.status(400).json({
-            message: result.message
+    try {
+        let { username, password } = req.body;
+        let result = adminValidations.parametersValidation(null, username, password);
+    
+        if(!result.allow) {
+            return res.status(400).json({
+                message: result.message
+            })
+        }
+    
+        let [rows] = await Admins.checkAdmin(username, password);
+    
+        res.status(200).json({
+            exitsAccount: (rows.length == 0) ? false : true
         })
+    } catch(error) {
+        res.status(500).json({
+            error: "[!] ERROR INTERNO DEL SERVIDOR"
+        })
+        console.log(`ERROR: ${error}`);
     }
+}
 
-    let [rows] = await Admins.checkAdmin(username, password);
+export const getAllAdminAccounts = async(req, res) => {
+    try {
+        let [rows] = await Admins.getAdmins();
 
-    res.status(200).json({
-        exitsAccount: (rows.length == 0) ? false : true
-    })
+        res.status(200).json({
+            accounts: rows
+        })
+    } catch(error) {
+        res.status(500).json({
+            error: "[!] ERROR INTERNO DEL SERVIDOR"
+        })
+        console.log(`ERROR: ${error}`);
+    }
 }
