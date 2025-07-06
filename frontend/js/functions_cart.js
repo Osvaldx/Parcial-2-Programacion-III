8cafd7cf3f5b3b6c759a9f7f76e2f701fe9a39fc
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-07-05 23:29:19 -0300

    add logic of showQuantity

--- frontend/js/functions_cart.js
+++ frontend/js/functions_cart.js
@@ -2,25 +2,63 @@ let itemsCart = document.getElementById("items-cart")
 let cart = JSON.parse(localStorage.getItem("cart"))
 
 const showCart = () => {
-    let product = ""
+    let showCartProduct = ""
     console.log(cart)
-    cart.forEach(p => {
-        product += `
+    for (let i = 0; i < cart.length; i++) {
+        let p = cart[i];
+        showCartProduct += `
         <li class="card-cart">
         <div class = "image-box">
-            <img src='${p.imagen}' alt="img-product">
+        <img src='${p.imagen}' alt="img-product">
         </div>
         <div id= "text-product">
-            <p>${p.nombre}</p>
-            <p>$${p.precio}</p>
+        <p>${p.nombre}</p>
+        <p>$${(p.precio * p.quantity)}</p>
+        </div>
+        <div class="quantity-selector">
+            <button class="minus">-</button>
+            <span class="quantity" data-index= "${i}">${p.quantity}</span>
+            <button class="plus">+</button>
         </div>
         <button><i class="fa-solid fa-trash"></i></button>
         </li>
         `
-    })
+    } 
+    
+    
+    itemsCart.innerHTML = showCartProduct;
+    showQuantity()
+    
 
-    itemsCart.innerHTML = product
 }
+
+let showQuantity = () => {
+    let quantitySpan = document.querySelectorAll('.quantity');
+    quantitySpan.forEach(span => {
+        let index = Number(span.dataset.index);
+        let product = cart[index];
+        let block = span.closest(".quantity-selector") // -> devuelve el primer elemento padre o el mismo elemento que encaja con el selector
+
+        // Evento botón -
+        let minusBtn = block.querySelector('.minus');
+        minusBtn.addEventListener('click', () => {
+            if (product.quantity > 1) {
+                product.quantity--;
+                localStorage.setItem("cart", JSON.stringify(cart));
+                showCart();
+            }
+        });
+        
+        // Evento botón +
+        let plusBtn = block.querySelector('.plus');
+        plusBtn.addEventListener('click', () => {
+            product.quantity++;
+            localStorage.setItem("cart", JSON.stringify(cart));
+            showCart();
+        });
+    })    
+}
+    
 
 
 const Init = () => {
