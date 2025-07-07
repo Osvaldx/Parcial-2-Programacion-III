c1443fdc0fe4de121ec7e7af7ab161eaaad26be3
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-07-06 22:48:26 -0300

    new function createAdmin

--- backend/src/public/js/functions_createAdmin.js
+++ backend/src/public/js/functions_createAdmin.js
@@ -1,11 +1,39 @@
+import parameters from "/js/adminParameterValidations.js";
 const ElementFormAdmin = document.querySelector(".box-form");
 
-const createAdmin = (event) => {
+const createAdminAPI = async(data) => {
+
+    let response = await fetch("http://localhost:3000/api/admin/createAccount", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+
+    return response
+}
+
+
+const createAdmin = async(event) => {
     event.preventDefault();
 
-    const form = new Form(event.target);
-    const data = Object.entries(form.entries());
+    let form = new FormData(event.target);
+    let data = Object.fromEntries(form.entries());
+    let result = parameters.parametersValidation(data.correo, data.nombre, data.contraseña)
 
+    if (result.allow) {
+        let response = await createAdminAPI(data)
+
+        if (response.ok) {
+            alert("Admin creado con exito")
+        }else{
+            alert("Error en el servidor")
+        }
+
+    }else{
+        alert(`${result.message}`)
+    }
 }
 
 ElementFormAdmin.addEventListener("submit", createAdmin)
