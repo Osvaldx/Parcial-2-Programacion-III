d9a960b210d165ca2403062542786d414db5c1f1
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-07-08 18:00:52 -0300

    logic added to remove an admin

---
+++ backend/src/public/js/functions_deleteAdmin.js
@@ -0,0 +1,38 @@
+import { validateID } from "./parametersValidations.js"
+
+let deleteAdminButtons = document.querySelectorAll(".deleteAdminButton")
+
+
+const deleteAdminAPI = async(id) => {
+    let response = fetch(`http://localhost:3000/api/admin/deleteAccount/${id}`, {
+        method: "DELETE"
+    })
+    
+    return response
+}
+
+let deleteAdmin = async(event) => {
+    const btn = event.target
+    let id_admin = btn.dataset.id_admin
+
+    let validate = validateID(id_admin)
+
+    if (validate) {
+        let response = await deleteAdminAPI(id_admin)
+
+        if(response.ok){
+            alert("ADMIN ELIMINADO CON EXITO")
+            window.location.reload(); // -> refresca la pagina
+        }else{
+            alert("NO SE PUDO ELIMINAR EL ADMIN")
+        }
+    }else{
+        alert("ERROR EN EL SERVIDOR AL ELIMINAR EL ADMIN")
+    }
+
+}
+
+
+deleteAdminButtons.forEach(button => {
+    button.addEventListener("click", deleteAdmin)
+})
