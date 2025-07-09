70b9a8559993871e2faa808e10c282866dc8865e
osvaldx <romanonahuel24@gmail.com>
2025-07-09 02:06:17 -0300

    add news routers

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,10 +1,14 @@
 import productRouter from "./product.routers.js";
 import viewRouter from "./views.routers.js";
 import adminRouter from "./adminAccount.router.js";
+import clientRouter from "./cliente.router.js";
+import ventaRouter from "./venta.router.js";
 
 // Archivo de barril con todas las rutas
 export {
     productRouter,
     viewRouter,
-    adminRouter
+    adminRouter,
+    clientRouter,
+    ventaRouter
 }
