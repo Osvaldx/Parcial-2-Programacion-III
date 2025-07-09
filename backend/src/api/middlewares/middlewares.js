d6dc8a5727cbcd6bb5b5829d65a47e2352280f15
osvaldx <romanonahuel24@gmail.com>
2025-07-09 02:06:31 -0300

    add news middlewares

--- backend/src/api/middlewares/middlewares.js
+++ backend/src/api/middlewares/middlewares.js
@@ -10,7 +10,76 @@ const validateID = (req, res, next) => {
     next();
 }
 
+const validateName = (req, res, next) => {
+    const { nombre } = req.body;
+    const regexName = new RegExp("^[a-zA-Z\\s]+$")
+
+    if((!regexName.test(nombre)) || !nombre) {
+        return res.status(404).json({
+            message: "[!] Nombre no valido"
+        })
+    }
+
+    next();
+}
+
+const validateSale = (req, res, next) => {
+    const { id_cliente, fecha_venta, total } = req.body;
+
+    if(!id_cliente || isNaN(id_cliente)) {
+        return res.status(404).json({
+            message: "[!] ID_CLIENTE INVALIDA"
+        })
+    }
+
+    if(!fecha_venta) {
+        return res.status(404).json({
+            message: "[!] FECHA INVALIDA"
+        })
+    }
+
+    if(!total || isNaN(total)) {
+        return res.status(404).json({
+            message: "[!] TOTAL INVALIDA"
+        })
+    }
+
+    next();
+}
+
+const validateSaleOfProducts = (req, res, next) => {
+    const { id_venta,id_product,cantidad,subtotal } = req.body;
+
+    if(!id_venta || isNaN(id_venta)) {
+        return res.status(404).json({
+            message: "[!] ID DE VENTA INVALIDA"
+        })
+    }
+
+    if(!id_product || isNaN(id_product)) {
+        return res.status(404).json({
+            message: "[!] ID DEL PRODUCTO INVALIDA"
+        })
+    }
+
+    if(!cantidad || isNaN(cantidad)) {
+        return res.status(404).json({
+            message: "[!] CANTIDAD INVALIDA"
+        })
+    }
+
+    if(!subtotal || isNaN(subtotal)) {
+        return res.status(404).json({
+            message: "[!] SUBTOTAL INVALIDO"
+        })
+    }
+
+    next();
+}
 
 export default {
-    validateID
+    validateID,
+    validateName,
+    validateSale,
+    validateSaleOfProducts
 }
