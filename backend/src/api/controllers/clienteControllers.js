12672af686522f1e058ee01b97461c1e53d98306
osvaldx <romanonahuel24@gmail.com>
2025-07-09 20:00:38 -0300

    add the return of id_cliente

--- backend/src/api/controllers/clienteControllers.js
+++ backend/src/api/controllers/clienteControllers.js
@@ -5,10 +5,18 @@ export const registerClient = async(req, res) => {
         const { nombre } = req.body;
 
         let [rows] = await Cliente.registerClientDB(nombre);
+
+        if(rows.affectedRows > 0) {
+            res.status(200).json({
+                message: "[+] Cliente registrado con exito",
+                id_cliente: rows.insertId
+            })
+        } else {
+            res.status(400).json({
+                message: "[!] Ocurrio un error al registrarse"
+            })
+        }
     
-        res.status(200).json({
-            message: (rows.affectedRows >= 1) ? "[+] Cliente registrado con exito" : "[!] Ocurrio un error al registrarse"
-        })
     } catch(error) {
         res.status(500).json({
             message: "[!] ERROR INTERNO DEL SERVIDOR"
