a2d610da496248fc85a167b954d2eeed3ce15fc0
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-07-09 21:16:24 -0300

    add logic to registerSale and registerSaleOfProd

--- frontend/js/functions_cart.js
+++ frontend/js/functions_cart.js
@@ -84,24 +84,21 @@ const showTotal = () => {
         total += (product.precio * product.quantity)
     })
 
-    const name = sessionStorage.getItem("username")
-
     view += `
     <section id = "total">
-        <div id = "cart-summary">
-            <h3>TOTAL: $${total}</h3>
-            <button id="confirmPurchase" data-cliente-nombre=${name}>Confirmar compra</button>
-            </div>
-            <div>
-            <button onclick = "clearCart()" id="clear">Vaciar carrito</button>
-        </div>
+    <div id = "cart-summary">
+    <h3>TOTAL: $${total}</h3>
+    <button id="confirmPurchase">Confirmar compra</button>
+    </div>
+    <div>
+    <button onclick = "clearCart()" id="clear">Vaciar carrito</button>
+    </div>
     </section>
     `
-
+    
     totalCart.innerHTML = view;
     const btnConfirmPurchase = document.getElementById("confirmPurchase");
     btnConfirmPurchase.addEventListener("click", cartConfirm);
-    btnConfirmPurchase.addEventListener("click", registerClient);
 }
 
 // ----------------BOTON BORRAR PRODUCTO-----------------------
@@ -109,7 +106,7 @@ const showTotal = () => {
 const deleteProduct = (id) => {
     let product = cart.find(product => product.id_product === id)
     let index = cart.indexOf(product) // -> da la posicion exacta del producto
-
+    
     if (product) {
         cart.splice(index, 1)
         sessionStorage.setItem("cart", JSON.stringify(cart))
@@ -125,20 +122,20 @@ const cartConfirm = () => {
         alert("TU CARRITO ESTA VACIO")
         return
     }
-
+    
     const { jsPDF } = window.jspdf;
-
+    
     // Creamos una nueva instancia del documento PDF usando la clase jsPDF
     const doc = new jsPDF()
-
+    
     let y = 10;
-
+    
     doc.setFontSize(14);
     doc.text("TICKET DE COMPRA - SkateSports", 20, y);
     y += 10;
     
     doc.setFontSize(12);
-
+    
     let totalTicket = 0;
     cart.forEach(product => {
         doc.text(`[NOMBRE]: ${product.nombre} - [CANTIDAD]: ${product.quantity} - [PRECIO x UNIDAD]: $${product.precio}`, 10, y)
@@ -146,20 +143,53 @@ const cartConfirm = () => {
         y+= 10;
     });
     doc.text(`[TOTAL DE LA COMPRA]: $${totalTicket}`, 10, y);
+    
+    sessionStorage.setItem("totalCart", totalTicket)
 
     doc.save("TICKET-COMPRA-SKATESPORT.pdf");
     setTimeout(() => {
         window.location.href = "../../index.html";
+        clearCart();
     }, 5000);
-
-
+    
+    registerClient()
 }
 
 // ----------------REGISTRAR CLIENTE-----------------------
 
 const registerClientAPI = async(data) => {
-
+    
     let response = await fetch(`http://localhost:3000/api/cliente/registrarCliente`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    
+    return response
+}
+
+const registerClient = async() => {
+    const clienteNombre = sessionStorage.getItem("username")
+    
+    let response = await registerClientAPI({nombre: clienteNombre})
+    let json = await response.json();
+    alert(json.id_cliente)
+
+    if (response.ok) {
+        alert("Cliente registrado con exito")
+    }else{
+        alert("ERROR AL REGISTRAR EL CLIENTE")
+    }
+
+    registerSale(json.id_cliente)
+}
+
+// ----------------REGISTRAR VENTA-----------------------
+const registerSaleAPI = async(data) => {
+
+    let response = await fetch(`http://localhost:3000/api/venta/registrarVenta`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -170,18 +200,49 @@ const registerClientAPI = async(data) => {
     return response
 }
 
-const registerClient = async(event) => {
-    const btn = event.target
-    let clienteNombre = btn.dataset.clienteNombre
 
-    let response = await registerClientAPI({nombre: clienteNombre})
+const registerSale = async(id_cliente) => {
+    const now = new Date();
+    const formattedDate = now.toISOString().slice(0, 19).replace('T', ' '); // Formato para enviarlo como un datatime a mySql
+    let totalCart = Number(sessionStorage.getItem("totalCart"))
+    let response = await registerSaleAPI({
+        "id_cliente": id_cliente,
+        "fecha_venta": formattedDate,
+        "total": totalCart
+    })
+    let json = await response.json();
+    registerSaleOfProd(json.id_venta)
+}
 
-    if (response.ok) {
-        alert("Cliente registrado con exito")
-        clearCart()
-    }else{
-        alert("ERROR AL REGISTRAR EL CLIENTE")
+// ----------------REGISTRAR VENTA PRODUCTO-----------------------
+const registerSaleOfProdAPI = async(data) => {
+
+    let response = await fetch("http://localhost:3000/api/venta/registrarVentaProd", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+
+    return response
+}
+
+const registerSaleOfProd = async(id_venta) => {
+
+    for (const prod of cart) {
+        let id_product = prod.id_product
+        let quantity = prod.quantity
+        let subtotal = (prod.precio * prod.quantity)
+        
+        let response = await registerSaleOfProdAPI({
+            "id_venta": id_venta,
+            "id_product": id_product,
+            "cantidad": quantity,
+            "subtotal": subtotal
+        })
     }
+
 }
 
 const Init = () => {
