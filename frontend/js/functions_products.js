b4d655f99901a69955efbe6a45cf76f1cff6cf6d
osvaldx <romanonahuel24@gmail.com>
2025-07-09 23:23:15 -0300

    the logic of the product lists has been improved

--- frontend/js/functions_products.js
+++ frontend/js/functions_products.js
@@ -10,6 +10,7 @@ let productRoller = document.getElementById("products-roller")
 let welcomeUser = document.getElementById("welcome-user")
 let cart = []
 let quantity = 0
+let allProducts = [];
 
 ///////////////
 // EVENTS //
@@ -32,9 +33,7 @@ cardRollers.addEventListener("click", function() {
 // agregar carrito
 
 const addCart = async(id) => {
-    console.log(id)
-    let allProducts = await getProductsAPI()
-    let product = allProducts['payload'].find(product => product.id_product === id)
+    let product = allProducts.find(product => product.id_product === id)
     let productCart = cart.find(p => p.id_product === id)
     if (productCart) {
         productCart.quantity += 1
@@ -52,11 +51,20 @@ const getProductsAPI = async () => {
     return data;
 }
 
+const getProducts = async () => {
+    try {
+        const data = await getProductsAPI();
+        allProducts = data.payload; // agarramos solo la lista
+    } catch (error) {
+        console.log(`[ERROR]: ${error}`)
+        allProducts = [];
+    }
+}
+
 const generateListProducts = async (typeProduct) => {
     let listProduct = [];
-    let allProducts = await getProductsAPI();
 
-    allProducts['payload'].forEach(product => {
+    allProducts.forEach(product => {
         if(product['categoria'] === typeProduct) {
             listProduct.push(product);
         }
@@ -67,31 +75,38 @@ const generateListProducts = async (typeProduct) => {
 
 const showProduct = (ElementShow, productList) => {
     let productsViews = "";
-    
-    for(let i = 0; i < productList.length; i++) {
-        let product = productList[i];
-        if(product.activo) {
-            productsViews += `
-            <li class="style-products">
-                <div class = "image-box">
-                <img src="${product.imagen}" alt="[!] Img product">          
-                </div>
-                <h3>${product.nombre}</h3>
-                <p>$${product.precio}</p>
-                <button onclick="addCart(${product.id_product})">Agregar al carrito</button>
-            </li>`
+    if(productList.length > 0) {
+        for(let i = 0; i < productList.length; i++) {
+            let product = productList[i];
+            if(product.activo) {
+                productsViews += `
+                <li class="style-products">
+                    <div class = "image-box">
+                    <img src="${product.imagen}" alt="[!] Img product">          
+                    </div>
+                    <h3>${product.nombre}</h3>
+                    <p>$${product.precio}</p>
+                    <button onclick="addCart(${product.id_product})">Agregar al carrito</button>
+                </li>`
+            }
         }
+    } else {
+        productsViews += "<h3>No hay productos disponibles</h3>"
     }
     
     ElementShow.innerHTML = productsViews;
 }
 
 const showAllProducts = async () => {
-    let skateProductList = await generateListProducts("SKATE");
+    let skateProductList = [];
+    let rollerProductList = [];
+
+    if(allProducts.length > 0) {
+        skateProductList = await generateListProducts("SKATE");
+        rollerProductList = await generateListProducts("ROLLER");
+    }
 
     showProduct(skateListElement, skateProductList);
-    let rollerProductList = await generateListProducts("ROLLER");
-
     showProduct(rollerListElement, rollerProductList);
 }
 
@@ -104,8 +119,9 @@ const validateUsername = () => {
     }
 }
 
-const Init = () => {
+const Init = async() => {
     validateUsername();
+    await getProducts();
     showAllProducts();
 
     const cartStored = JSON.parse(sessionStorage.getItem("cart")) //Pasamos el String a objeto 
